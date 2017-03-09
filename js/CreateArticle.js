48d5e7dd5e0df1b44181ec0521b8c3d942232849
Antecer <ymh441915964@gmail.com>
2017-03-09 14:17:08 +0800

    14:17:06

--- js/CreateArticle.js
+++ js/CreateArticle.js
@@ -18,20 +18,26 @@ String.prototype.format = function() {
 		});
 }
 
+var json = {
+	"title": "TestCreateIssues",
+	"body": "测试一下post创建issues",
+	"assignee": "Antecer",
+	"milestone": null,
+	"labels": ""
+}
 
 $(document).ready(function() {
 	$("#postIssues").click(function() {
-		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("POST", issues.url, true);
-		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xmlhttp.send(
-			'{ "title": "TestCreateIssues", "body": "测试一下post创建issues", "assignee": "Antecer", "assignees": [{ "login": "Antecer", "id": 8684916, "avatar_url": "https://avatars2.githubusercontent.com/u/8684916?v=3", "gravatar_id": "", "url": "https://api.github.com/users/Antecer", "html_url": "https://github.com/Antecer", "followers_url": "https://api.github.com/users/Antecer/followers", "following_url": "https://api.github.com/users/Antecer/following{/other_user}", "gists_url": "https://api.github.com/users/Antecer/gists{/gist_id}", "starred_url": "https://api.github.com/users/Antecer/starred{/owner}{/repo}", "subscriptions_url": "https://api.github.com/users/Antecer/subscriptions", "organizations_url": "https://api.github.com/users/Antecer/orgs", "repos_url": "https://api.github.com/users/Antecer/repos", "events_url": "https://api.github.com/users/Antecer/events{/privacy}", "received_events_url": "https://api.github.com/users/Antecer/received_events", "type": "User", "site_admin": false }], "milestone": 1, "labels": ["bug"] }'
-			);
-//		$.post(issues.url.format(issues.author, issues.project), 
-//		{ "title": "TestCreateIssues", "body": "测试一下post创建issues", "assignee": "Antecer", "assignees": [{ "login": "Antecer", "id": 8684916, "avatar_url": "https://avatars2.githubusercontent.com/u/8684916?v=3", "gravatar_id": "", "url": "https://api.github.com/users/Antecer", "html_url": "https://github.com/Antecer", "followers_url": "https://api.github.com/users/Antecer/followers", "following_url": "https://api.github.com/users/Antecer/following{/other_user}", "gists_url": "https://api.github.com/users/Antecer/gists{/gist_id}", "starred_url": "https://api.github.com/users/Antecer/starred{/owner}{/repo}", "subscriptions_url": "https://api.github.com/users/Antecer/subscriptions", "organizations_url": "https://api.github.com/users/Antecer/orgs", "repos_url": "https://api.github.com/users/Antecer/repos", "events_url": "https://api.github.com/users/Antecer/events{/privacy}", "received_events_url": "https://api.github.com/users/Antecer/received_events", "type": "User", "site_admin": false }], "milestone": 1, "labels": ["bug"] },
-//			function(data) {
-//				$("#comment").empty();
-//				$("#comment").append(data);
-//			});
+		$.ajax({
+			headers: { Accept: "application/vnd.github.squirrel-girl-preview.full+json" },
+			dataType: "json",
+			type: "post",
+			url: issues.url.format(issues.author, issues.project),
+			async: true,
+			data: json,
+			success: function() {
+				$("#comment").append(data);
+			}
+		});
 	});
 });
