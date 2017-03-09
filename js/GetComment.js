b8f6c27d6d2adf1285dbb162d80d7944e5ea06d5
Antecer <ymh441915964@gmail.com>
2017-03-09 10:36:47 +0800

    2017-03-09

--- js/GetComment.js
+++ js/GetComment.js
@@ -1,5 +1,3 @@
-document.write("<script language='javascript' src='jquery-3.1.1.js'></script>");
-
 $(document).ready(function(){
     $("#comment").load("https://github.com/farseerfc/farseerfc.github.io/issues/52# .js-discussion");
 });
