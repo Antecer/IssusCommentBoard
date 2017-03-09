6fdfeb90f24eb60cae1c6a244c838605af7bd4ec
Antecer <ymh441915964@gmail.com>
2017-03-09 13:45:45 +0800

    13:45:42

--- js/CreateArticle.js
+++ js/CreateArticle.js
@@ -13,7 +13,7 @@ var issues = {
 $(document).ready(function() {
 	$("#postIssues").click(function() {
 		var xmlhttp = new XMLHttpRequest();
-		xmlhttp.open("POST", "issues.url.format(issues.author, issues.project", true);
+		xmlhttp.open("POST", issues.url.format(issues.author, issues.project), true);
 		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		xmlhttp.send(
 			'{ "title": "TestCreateIssues", "body": "测试一下post创建issues", "assignee": "Antecer", "assignees": [{ "login": "Antecer", "id": 8684916, "avatar_url": "https://avatars2.githubusercontent.com/u/8684916?v=3", "gravatar_id": "", "url": "https://api.github.com/users/Antecer", "html_url": "https://github.com/Antecer", "followers_url": "https://api.github.com/users/Antecer/followers", "following_url": "https://api.github.com/users/Antecer/following{/other_user}", "gists_url": "https://api.github.com/users/Antecer/gists{/gist_id}", "starred_url": "https://api.github.com/users/Antecer/starred{/owner}{/repo}", "subscriptions_url": "https://api.github.com/users/Antecer/subscriptions", "organizations_url": "https://api.github.com/users/Antecer/orgs", "repos_url": "https://api.github.com/users/Antecer/repos", "events_url": "https://api.github.com/users/Antecer/events{/privacy}", "received_events_url": "https://api.github.com/users/Antecer/received_events", "type": "User", "site_admin": false }], "milestone": 1, "labels": ["bug"] }'
